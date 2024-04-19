938d24dbf5df52030704d5be1309b1a58bd0565e
mohitsharma <mohittsharma475@gmail.com>
2024-04-19 21:17:14 +0530

    step component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,43 @@
+import { useState } from "react";
+import "./App.css";
 
-
-
-import { useState } from "react"
-import "./App.css"
-
-
-
-const messages = [
-  "Learn ReactJS",
-  "Learn NodeJS",
-  "Learn MongoDB"
-]
-
+const messages = ["Learn ReactJS", "Learn NodeJS", "Learn MongoDB"];
 
 function App() {
+  const [step, setStep] = useState(1);
+  const [test, setTest] = useState({ name: "jonhs" });
 
-  const [step,setStep]  =  useState(1);
-  const [test] =  useState({name:"jonhs"})
-
-  function handlePrev(){
-    if(step>1)
-      setStep(step-1);
-
+  function handlePrev() {
+    if (step > 1) setStep(step - 1);
   }
-  function handleNext(){
-    if(step<3)
-    setStep(step+1)
+  function handleNext() {
+    if (step < 3) setStep(step + 1);
     //never do it
-    test.name = "mohit"
+    // test.name = "mohit"
 
+    setTest({ name: "mohit" });
   }
-
-
 
   // const step = 0;
-  
+
   return (
     <div className="steps">
       <div className="numbers">
-        <div className={`${step===1 ? "active":""}`}>1</div>
-        <div className={`${step===2 ? "active":""}`} >2</div>
-        <div className={`${step===3 ? "active":""}`}>3</div>
+        <div className={`${step === 1 ? "active" : ""}`}>1</div>
+        <div className={`${step === 2 ? "active" : ""}`}>2</div>
+        <div className={`${step === 3 ? "active" : ""}`}>3</div>
       </div>
 
-      <div className="message">Step {step}: {messages[step-1]} {test.name}</div>
+      <div className="message">
+        Step {step}: {messages[step - 1]} {test.name}
+      </div>
 
       <div className="buttons">
         <button onClick={handlePrev}>Previous</button>
         <button onClick={handleNext}>Next</button>
       </div>
-
-
-
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
